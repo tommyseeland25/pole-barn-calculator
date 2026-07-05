b7f77e31405643cdfbd27aa163c9b85558f36e7b
tommyseeland25 <tommyseeland@gmail.com>
2026-07-05 16:55:31 -0400

    Added number of truss logic

--- script.js
+++ script.js
@@ -8,16 +8,20 @@ document.addEventListener("DOMContentLoaded", function() {
   const widthInput = document.getElementById("width");
   const lengthInput = document.getElementById("length");
   const heightInput = document.getElementById("height");
+  const roofPitchInput = document.getElementById("roofPitch");
+  const trussSpacingInput = document.getElementById("trussSpacing");
 
   btn.addEventListener("click", function() {
     const widthValue = Number(widthInput.value);
     const lengthValue = Number(lengthInput.value);
     const heightValue = Number(heightInput.value);
+    const roofPtichValue = Number(roofPitchInput.value);
+    const trussSpacingValue = Number(trussSpacingInput.value);
 
     // storing to log, F12
-    console.log(widthValue, lengthValue, heightValue);
+    //console.log(widthValue, lengthValue, heightValue, roofPtichValue);
 
-    // fllor area calculation
+    // floor area calculation
     const floorArea = widthValue * lengthValue;
     console.log("Floor area:", floorArea);
 
@@ -36,12 +40,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const num_2x6x16_boards= Math.ceil(total_girt_length / 16);
     console.log("Girt Boards (2x6x16):", num_2x6x16_boards);
 
+    // Roof logic
+    console.log("Roof Pitch", roofPtichValue);
+    console.log("Truss Spacing", trussSpacingValue);
+    const num_of_trusses = (lengthValue / trussSpacingValue) + 1;
+    //const rafter
+
     // words presented to user
     document.getElementById("results").innerHTML = 
     "Floor Area: " + floorArea + " sq ft" +
     "<br>Wall Area: " + wallArea + " sq ft" +
     "<br>Number of Posts: " + numPosts +
-    "<br>Girt Boards (2x6x16): " + num_2x6x16_boards;
+    "<br>Girt Boards (2x6x16): " + num_2x6x16_boards +
+    "<br>Number of Trusses: " + num_of_trusses;
 
   });
 });
